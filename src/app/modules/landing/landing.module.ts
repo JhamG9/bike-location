fe6c2f20a4a1e3cade7d0e53dc12bf308d8c17fb
Jhampier Giron <jose.giron@soytul.com>
2023-01-22 02:27:48 -0500

    feat: 🎸 added nav header
    added nav header

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing-rounting.module';
+import { NavbarLandingComponent } from './components/navbar-landing/navbar-landing.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    LandingComponent
-  ],
-  imports: [
-    CommonModule,
-    LandingRoutingModule
-  ]
+  declarations: [LandingComponent, NavbarLandingComponent],
+  imports: [CommonModule, LandingRoutingModule, MatIconModule],
 })
-export class LandingModule { }
+export class LandingModule {}
