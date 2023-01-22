5fdc0f8e606896d7940b01338004dba845aa16a2
Jhampier Giron <jose.giron@soytul.com>
2023-01-22 02:47:16 -0500

    feat: 🎸 added menu mobile
    added drawer menu mobile

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -4,9 +4,16 @@ import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing-rounting.module';
 import { NavbarLandingComponent } from './components/navbar-landing/navbar-landing.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
 
 @NgModule({
-  declarations: [LandingComponent, NavbarLandingComponent],
-  imports: [CommonModule, LandingRoutingModule, MatIconModule],
+  declarations: [LandingComponent, NavbarLandingComponent, MenuMobileComponent],
+  imports: [
+    CommonModule,
+    LandingRoutingModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
 })
 export class LandingModule {}
