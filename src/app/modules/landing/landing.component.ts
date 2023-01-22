aa0d7862b4006d53443f198c8dc96eed0740712d
Jhampier Giron <jose.giron@soytul.com>
2023-01-22 01:13:01 -0500

    feat: 🎸 added test get
    test get dad

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -4,16 +4,15 @@ import { UserFirestoreService } from 'src/app/firebase/user-firestore.service';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-
-  constructor(private userFirestoreService: UserFirestoreService) { }
+  users = null;
+  constructor(private userFirestoreService: UserFirestoreService) {}
 
   ngOnInit(): void {
-    this.userFirestoreService.getAllUsers().subscribe((resp) =>{
-      console.log(resp);
+    this.userFirestoreService.getAllUsers().subscribe((resp: any) => {
+      this.users = resp;
     });
   }
-
 }
