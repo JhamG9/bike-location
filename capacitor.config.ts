fd38d775584408a0c8fb972b903584ea87fbadb7
Jhampier Giron <jose.giron@soytul.com>
2023-01-23 11:01:46 -0500

    fix: 🐛 changed route deploy
    changed route deploy

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'angular-mobile-app',
-  webDir: 'dist/angular-mobile-app',
+  webDir: 'docs',
   bundledWebRuntime: false
 };
 
